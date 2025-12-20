87947ccf4de6afff0d74fd7618d06920ecd991c5
devSiddharthKarn <192719686+devSiddharthKarn@users.noreply.github.com>
2025-12-20 22:09:57 +0545

    added code shot

--- test/test.ts
+++ test/test.ts
@@ -1,19 +1,19 @@
 import { Mango } from "../src/mango.js";
 
 //create mango 🥭instance
-const mango:Mango = new Mango();
+const mango: Mango = new Mango();
 
-try{
+try {
     //initialize the basic connection params
     await mango.connect({
-        host: process.env.HOST ,
-        user: process.env.USER ,
-        password: process.env.PASSWORD ,
-        database: process.env.DB_NAME 
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DB_NAME
     })
     console.log("Connected to Sql DB success");
-}catch(error){
-    console.log("Oops!!! Connection to DB failed, error:",error);
+} catch (error) {
+    console.log("Oops!!! Connection to DB failed, error:", error);
 }
 
 //create Users table (by default Users is lowercased to users in db)
@@ -30,16 +30,15 @@ const Users = mango.selectTable("Users");
 
 //need to await
 await Users.insertOne({
-    username:"Siddharth Karna",
-    password:"siddharth",
-    email:"siddharth@gmail.com",
+    username: "Siddharth Karna",
+    password: "siddharth",
+    email: "siddharth@gmail.com",
 }).execute();
 
 
 ///get data
-const users =await Users.selectColumns(["username","email","password"]).execute();
+const users = await Users.selectColumns(["username", "email", "password"]).execute();
 
 //close connection
 
 await mango.disconnect();
-    
